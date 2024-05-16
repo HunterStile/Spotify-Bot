2a9b3ed5d5a7c716ce7ec0752337ea2c5210ccf7
Luigi Delle Cave <80790907+HunterStile@users.noreply.github.com>
2024-05-17 00:36:16 +0200

    Add file via upload

---
+++ Only_creazione.py
@@ -0,0 +1,25 @@
+from spotify_functions import *
+
+## >main<
+ripetizione = True
+count = 0
+
+while ripetizione == True:
+    count = count + 1
+    print(count,"*account in creazione")
+    Crea_e_Segui()
+    #input("Fai Quello che devi..")
+    ripetizione == True
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ boisting_creazione_spam.py
@@ -0,0 +1,22 @@
+from spotify_functions import *
+
+## >main<
+
+while True:
+    count = count + 1
+    print(count,'° creazione')
+    Crea_e_spamma()
+    sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
