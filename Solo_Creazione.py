efecc6a4a78a6ee32dd53242c276c26958862d5a
HunterStile <you@example.com>
2024-05-17 02:32:58 +0200

    modifica dell applicativo solo_creazione

---
+++ Solo_Creazione.py
@@ -0,0 +1,26 @@
+from funzioni.spotify_functions import *
+
+## >main<
+ripetizione = True
+count = 0
+
+while ripetizione == True:
+    count = count + 1
+    print(count,"*account in creazione")
+    driver = configurazione_browser()
+    crea_account(driver)
+    input("Fai Quello che devi..")
+    ripetizione == True
+
+
+
+
+
+
+
+
+
+
+
+
+
