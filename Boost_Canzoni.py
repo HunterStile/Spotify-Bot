dc83aa62c7c795472c50b50875d0abfb6c8b752e
HunterStile <you@example.com>
2024-05-19 22:41:08 +0200

    Modifica all'applicativo

--- Boost_Canzoni.py
+++ Boost_Canzoni.py
@@ -12,11 +12,13 @@ all_posizione = [posizione8,posizione9,posizione10,posizione11,posizione12,posiz
 #top-music - PLAYLIST5
 #inde-italia - PLAYLIST6
 #start
-file = 'account_spotify_creati.csv'
+
+file = 'account_spotify.csv'
 with open(file, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)  # salta la prima riga con l'intestazione
     for row in csvreader:
+        posizione_casuale = random.choice(all_posizione)
         count = count + 1
         print(count,'° Account')
         email = row[0]
@@ -25,11 +27,8 @@ with open(file, newline='') as csvfile:
         errore = Accesso_spotify(driver,email,password)
         if errore == False:
             scegli_playlist(driver,playlist1) #poppunk
-            Sento_canzone(driver,posizione5)  
-            Sento_canzone(driver,posizione7)
             Sento_canzone(driver,posizione11)
-            Sento_canzone(driver,posizione16)
-            Sento_canzone(driver,posizione20)
+            sleep(120)
             
         else:
             print("Accesso non riusciuto, riprovo con il prossimo account...")
