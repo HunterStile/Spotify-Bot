14f0725947ffd4d3f7a1f25911f77670cc24990f
HunterStile <you@example.com>
2024-06-12 07:06:07 +0200

    Aggiornato il file config

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ playlist4='https://open.spotify.com/playlist/110gM33fjQUsNASKxTG4LV'  #new-music
 playlist5='https://open.spotify.com/playlist/2EAacye5AQOZu9WN8Z5k4X'  #top-music
 playlist6='https://open.spotify.com/playlist/52DGNZbiGch7ewRJOmVuLS'  #Indieitalia
 
+#Per l'applicativo boosting_canzone
+file = 'account_spotify.csv'
