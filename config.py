94ddaab8b593c4eccfb0e65434493c9d6808e2d1
HunterStile <luigidellecave7@gmail.com>
2024-11-29 20:49:08 +0100

    nuovo deafult

--- config.py
+++ config.py
@@ -1,60 +1,69 @@
 import random
 
-POSIZIONI_DISPONIBILI = ['1', '2', '3', '4','5']
-MIN_POSIZIONI = 1
-MAX_POSIZIONI = 3
+# Scegli se eseguere una nuova creazione con True, accessi ad account gia creati scritti in account_spotify.csv con False 
+CREAZIONE = True
+#scegliere True per usare proxy, False senza.
+PROXY = False
+#scegliere se abilitare la routine di seguire le playlist
+SEGUI_PLAYLIST = True
+#scegliere se ablitare la routine di ascoltare canzoni
+ASCOLTA_CANZONI = True
+
+#SCELTA DELLA PLAYLIST DOVE ASCOLTARE LE CANZONI
+PLAYLIST_SCELTA = [
+    'https://open.spotify.com/playlist/1TmUjkWHXsKgTsIKvJiCJC',   # Pop-Punk
+]
+POSIZIONI_SCELTE = ['3','4']
+MIN_POSIZIONI = 2
+MAX_POSIZIONI = 2
 MIN_RIPETIZIONI = 1
 MAX_RIPETIZIONI = 1
+INTERVALLO_ASCOLTO = random.randint(120, 150)
+
+# SCELTA DELLE PLAYLIST
+PLAYLIST_URLS = [
+    'https://open.spotify.com/playlist/1TmUjkWHXsKgTsIKvJiCJC',   # Pop-Punk
+    'https://open.spotify.com/playlist/1qEvrxdkHTJdrtxHlG80Ry',   # Trap Italia
+    'https://open.spotify.com/playlist/55aJEEvbqcQNF2MRQncP5R',   # New Generation
+    'https://open.spotify.com/playlist/110gM33fjQUsNASKxTG4LV',   # New Music
+    'https://open.spotify.com/playlist/2EAacye5AQOZu9WN8Z5k4X',   # Top Music
+    'https://open.spotify.com/playlist/52DGNZbiGch7ewRJOmVuLS',   # Indie Italia
+    'https://open.spotify.com/playlist/77EE22OcfpL3fL7wDkagpX',
+    'https://open.spotify.com/playlist/6ke0AGVivZiVVUkqSHWTAQ',
+    'https://open.spotify.com/playlist/1CZFCCGYLzYh1BpxAXHy4b',
+    'https://open.spotify.com/playlist/1Hgl2BCI250xGtyGFXuJpB',
+    'https://open.spotify.com/playlist/3F82VLnmyuQ0ujPQPnxdRy'
+]
 
 
 configurazione_bot = {
     # Impostazioni generali
-    'crea_account': True,
+    'crea_account': CREAZIONE,
     'max_iterazioni': 3,
     'input_utente': False,
     'ripetizione': True,
 
     # Configurazione Proxy
-    'usa_proxy': False,  # Abilita/disabilita l'uso dei proxy
+    'usa_proxy': PROXY,  # Abilita/disabilita l'uso dei proxy
     'proxy_list': [
         'profilo1.ppx',
         'profilo2.ppx'
     ],
     
     # Configurazione playlist da seguire
-    'segui_playlist': True,
-    'playlist_urls': [
-        'https://open.spotify.com/playlist/1TmUjkWHXsKgTsIKvJiCJC',   # Pop-Punk
-        'https://open.spotify.com/playlist/1qEvrxdkHTJdrtxHlG80Ry',   # Trap Italia
-        'https://open.spotify.com/playlist/55aJEEvbqcQNF2MRQncP5R',   # New Generation
-        'https://open.spotify.com/playlist/110gM33fjQUsNASKxTG4LV',   # New Music
-        'https://open.spotify.com/playlist/2EAacye5AQOZu9WN8Z5k4X',   # Top Music
-        'https://open.spotify.com/playlist/52DGNZbiGch7ewRJOmVuLS',    # Indie Italia
-        'https://open.spotify.com/playlist/77EE22OcfpL3fL7wDkagpX',
-        'https://open.spotify.com/playlist/6ke0AGVivZiVVUkqSHWTAQ',
-        'https://open.spotify.com/playlist/1CZFCCGYLzYh1BpxAXHy4b',
-        'https://open.spotify.com/playlist/1Hgl2BCI250xGtyGFXuJpB',
-        'https://open.spotify.com/playlist/3F82VLnmyuQ0ujPQPnxdRy'      
-    ],
+    'segui_playlist': SEGUI_PLAYLIST,
+    'playlist_urls': PLAYLIST_URLS,
     
     # Configurazione ascolto canzoni
-    'ascolta_canzoni': True,
-    'playlist_ascolto': random.choice([
-        'https://open.spotify.com/playlist/1TmUjkWHXsKgTsIKvJiCJC',   
-        'https://open.spotify.com/playlist/1qEvrxdkHTJdrtxHlG80Ry',   
-        'https://open.spotify.com/playlist/55aJEEvbqcQNF2MRQncP5R',   
-        'https://open.spotify.com/playlist/110gM33fjQUsNASKxTG4LV',   
-        'https://open.spotify.com/playlist/2EAacye5AQOZu9WN8Z5k4X',   
-        'https://open.spotify.com/playlist/52DGNZbiGch7ewRJOmVuLS'
-    ]),
-    
+    'ascolta_canzoni': ASCOLTA_CANZONI,
+    'playlist_ascolto': random.choice(PLAYLIST_SCELTA),
     # Posizioni e ripetizioni di ascolto casuali
-    'posizioni_ascolto': random.sample(POSIZIONI_DISPONIBILI, k=random.randint(MIN_POSIZIONI, MAX_POSIZIONI)),
+    'posizioni_ascolto': random.sample(POSIZIONI_SCELTE, k=random.randint(MIN_POSIZIONI, MAX_POSIZIONI)),
     'ripetizioni_per_posizione': {
         posizione: random.randint(MIN_RIPETIZIONI, MAX_RIPETIZIONI) 
-        for posizione in random.sample(POSIZIONI_DISPONIBILI, k=random.randint(MIN_POSIZIONI, MAX_POSIZIONI))
+        for posizione in random.sample(POSIZIONI_SCELTE, k=random.randint(MIN_POSIZIONI, MAX_POSIZIONI))
     },
     
     # Intervallo tra gli ascolti
-     'intervallo_ascolto': random.randint(120, 150)
+     'intervallo_ascolto': INTERVALLO_ASCOLTO
 }
