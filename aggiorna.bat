a45d0af881863c70fb759dd4aa910d79ccf02f11
HunterStile <luigidellecave7@gmail.com>
2024-12-05 01:19:17 +0100

    Aggiornamento autoamtico(1 rpoa

---
+++ aggiorna.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /D "C:\Users\Luigi\Desktop\GITHUB\Spotify-Bot"
+git pull
+pause
--- config.py
+++ config.py
@@ -9,8 +9,8 @@ DOPPIOPROXY = False
 STOP_FOR_ROBOT = True
 TEMPO_RIPARTENZA = 7200
 #Gestione del reset
-RESET_ROUTER = True
-TIPO_ROUTER = 'vodafone'
+RESET_ROUTER = False
+TIPO_ROUTER = 'tim'
 #scegliere se abilitare la routine di seguire le playlist
 SEGUI_PLAYLIST = True
 #scegliere se ablitare la routine di ascoltare canzoni
