15f83ca3a2d2ca3f84105305725e5274aa714e35
HunterStile <luigidellecave7@gmail.com>
2024-12-05 16:33:51 +0100

    aggiornato ancora meglio il comando aggiornamento

--- aggiorna.bat
+++ aggiorna.bat
@@ -1,8 +1,13 @@
 @echo off
 cd /D "C:\Users\PC\Desktop\Spotify-Bot"
 
-:: Scarta le modifiche locali non commesse
-git checkout -- .
+:: Controlla se ci sono modifiche locali non commesse
+git diff --quiet
+if %ERRORLEVEL% NEQ 0 (
+    echo Ci sono modifiche locali non commesse. Le modifiche verranno scartate.
+    pause
+    git checkout -- .
+)
 
 :: Esegui il pull della repository
 git pull
